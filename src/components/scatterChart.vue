70c243d1948f59c163fd0070c41ceb8294bf823b
Seb-klay <92079542+Seb-klay@users.noreply.github.com>
2022-01-18 22:28:13 +0100

    améliorations des fonctionnalités

---
+++ src/components/scatterChart.vue
@@ -0,0 +1,63 @@
+<script>
+import { Scatter } from "vue-chartjs";
+
+export default {
+  props: ["x", "y"],
+  extends: Scatter,
+  data() {
+    return {
+      chartData: {
+        datasets: [
+          {
+            label: "Etoile recherchée",
+            borderWidth: 3,
+            radius: 10,
+            borderColor: "yellow",
+            pointStyle: "star",
+            data: [
+              {
+                x: this.x,
+                y: this.y,
+              },
+            ],
+          },
+        ],
+      },
+      options: {
+        scales: {
+          xAxes: [
+            {
+              ticks: {
+                min: 0,
+                max: 24,
+                stepSize: 1,
+                reverse: true,
+                beginAtZero: true,
+              },
+            },
+          ],
+          yAxes: [
+            {
+              ticks: {
+                min: -90,
+                max: 90,
+                stepSize: 10,
+                reverse: false,
+                beginAtZero: false,
+              },
+            },
+          ],
+        },
+        legend: {
+          display: true,
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+      },
+    };
+  },
+  mounted() {
+    this.renderChart(this.chartData, this.options);
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -3,13 +3,14 @@ import axios from "axios";
 import App from "./App.vue";
 import Vue from "vue";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import VueIframe from "vue-iframes";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
-// Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
+Vue.use(VueIframe);
 
 Vue.config.productionTip = false;
 
